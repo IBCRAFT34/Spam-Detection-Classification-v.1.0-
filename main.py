5e5458eb6b382ce57866f17d7913450d97182677
IBCRAFT34 <166202280+IBCRAFT34@users.noreply.github.com>
2025-04-12 17:20:06 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Spam Detection Programs")
+
+st.header("Login to Your Email Please...>w<!!")
+emailAddress = st.text_input("Email Address", placeholder="Enter your email")
+password = st.text_input("Password", placeholder="Enter your password", type="password")
+
+st.button('Logi here >w<')
